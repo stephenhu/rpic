585450d07cf8ebbc00aac79b1c37d296e82c46c0
stephenhu <epynonymous@outlook.com>
2022-02-17 13:41:25 +0000

    sidebar initial draft

--- www/js/rpic.js
+++ www/js/rpic.js
@@ -1,10 +1,11 @@
 /* rpic.js */
 
 const API                 = "/api";
+const API_AUTH            = "/auth";
 const API_NETWORKS        = "/networks";
 const API_SERVICES        = "/services";
 const API_SYSTEM          = "/systems";
-const API_AUTH            = "/auth";
+const API_USERS           = "/users";
 
 const ID_USER             = "user";
 const ID_PASS             = "pass";
@@ -13,33 +14,156 @@ const HTTP_DELETE         = "delete";
 const HTTP_GET            = "get";
 const HTTP_PUT            = "put";
 
+const HTTP_200            = 200;
 const HTTP_401            = 401;
 
 const ERR_LOGIN_FAILED    =
   "Login credentials are invalid";
 
+const ATTR_ID             = "id";
+
 const DBLOCK              = "d-block";
+const DFLEX               = "d-flex";
 const DNONE               = "d-none";
 
-const PAGE_HOME           = "home";
-const PAGE_LOGIN          = "login";
+const VIEW_APP            = "app";
+const VIEW_HOME           = "home";
+const VIEW_LOGIN          = "login";
+const VIEW_MAIN           = "main";
+const VIEW_SIDEBAR        = "sidebar";
+
+const VIEW_DASHBOARD      = "dashboard";
+const VIEW_NETWORK        = "network";
+const VIEW_STORAGE        = "storage";
+const VIEW_SERVICES       = "services";
+const VIEW_LOGS           = "logs";
+const VIEW_WIREGUARD      = "wireguard";
+
+const NULL_STRING         = "";
 
 
-function togglePage(s, h) {
+function parseIds(r) {
 
-  var show = document.getElementById(s);
-  var hide = document.getElementById(h);
+  var root = document.getElementById(r);
 
-  show.classList.remove(DNONE);
-  show.classList.add(DBLOCK);
+  if(root === undefined || root === null) {
+    console.log("Not found");
+  } else {
 
-  hide.classList.remove(DBLOCK);
-  hide.classList.add(DNONE);  
+    var ids = new Array();
 
-} // togglePage
+    for(var i = 0; i < root.children.length; i++) {
+      ids.push(root.children[i].getAttribute(ATTR_ID));
+    }
+
+    return ids;
+  
+  }
+
+} // parseIds
 
 
-function login() {
+function getTagType(t) {
+
+  if(t === DNONE || t === DBLOCK || t === DFLEX) {
+    return t;
+  } else {
+
+    console.log("Invalid display type, defaulting to DBLOCK: " + t);
+    return DBLOCK;
+
+  }
+
+} // getTagType
+
+
+function showFromList(l, v, t) {
+
+  let ids = parseIds(l);
+
+  for(var i = 0; i < ids.length; i++) {
+
+    if(ids[i] === v) {
+      showView(ids[i], t);
+    } else {
+      hideView(ids[i], t);
+    }
+
+  }
+
+} // showFromList
+
+
+function hideList(v, t) {
+
+  let ids = parseIds(VIEW_MAIN);
+
+  for(var i = 0; i < ids.length; i++) {
+    hideView(ids[i], t);
+  }
+
+} // hideList
+
+
+function showView(v, t) {
+
+  var e = document.getElementById(v);
+  
+  e.classList.remove(DNONE);
+
+  let tag = getTagType(t);
+
+  e.classList.add(tag);
+
+} // showView
+
+
+function hideView(v, t) {
+
+  var e = document.getElementById(v);
+  
+
+  let tag = getTagType(t);
+
+
+  e.classList.remove(tag);
+  e.classList.add(DNONE);
+
+} // hideView
+
+
+function checkAuth() {
+
+  window.addEventListener("hashchange", hashtagListener);
+
+  fetch(`${API}${API_USERS}`, {
+    method: HTTP_GET,
+  })
+  .then((response) => {
+
+    if(response.status === HTTP_200) {
+
+      hashtagListener();
+
+    } else if(response.status === HTTP_401) {
+      
+      hideView(VIEW_APP, DFLEX);
+      showView(VIEW_LOGIN, DBLOCK);
+
+    }
+
+  })
+  .then((data) => {
+
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+} // checkAuth
+
+
+function signin() {
   
   var user = document.getElementById(ID_USER);
   var pass = document.getElementById(ID_PASS);
@@ -63,7 +187,7 @@ function login() {
       }
       
     } else {
-      togglePage(PAGE_HOME, PAGE_LOGIN);
+      toggleView(VIEW_MAIN, VIEW_LOGIN);
     }
 
   })
@@ -74,10 +198,10 @@ function login() {
     alert(ERR_LOGIN_FAILED);
   })
 
-} // login
+} // signin
 
 
-function logout() {
+function signout() {
 
   fetch(`${API_AUTH}`, {
     method: HTTP_DELETE,
@@ -86,6 +210,10 @@ function logout() {
     console.log(response);
     if(response.ok) {
       
+      if(response.status === HTTP_200) {
+        toggleView(VIEW_LOGIN, VIEW_MAIN);
+      }
+
     }
   })
   .then((data) => {
@@ -95,7 +223,7 @@ function logout() {
     console.log(error);
   })
 
-} // logout
+} // signout
 
 
 function reboot() {
@@ -153,3 +281,53 @@ function serviceCommand(c) {
   })
 
 } // serviceCommand
+
+
+function hashtagListener() {
+
+  var hash = new URL(document.URL).hash.substring(1);
+
+  if(hash === NULL_STRING) {
+    
+    hideView(VIEW_LOGIN, DBLOCK);
+    showView(VIEW_APP, DFLEX);
+    
+    showFromList(VIEW_MAIN, VIEW_DASHBOARD, DBLOCK);
+
+  } else {
+
+    showView(VIEW_APP, DFLEX);
+
+    showFromList(VIEW_MAIN, hash, DBLOCK);
+
+  }
+
+} // hashtagListener
+
+
+/*
+function init(pages) {
+
+  window.onload = function () {
+
+    var main    = document.getElementById(VIEW_MAIN);
+    var login   = document.getElementById(VIEW_LOGIN);
+
+    var hash = new URL(document.URL).hash;
+
+    if(hash === EMPTY) {
+
+      hideView(VIEW_LOGIN, DBLOCK);
+      
+      showFromList(VIEW_APP, VIEW_DASHBOARD, DFLEX);
+
+    } else {
+      hashtagListener();
+    }
+
+  }
+
+  window.addEventListener("hashchange", hashtagListener);
+
+} // init
+*/
